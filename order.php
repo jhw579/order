23b9e68a7713994702682a1bc9ce927c72f0a921
jhw579 <39336789+jhw579@users.noreply.github.com>
2018-05-30 08:00:44 +0800

    Update order.php

--- order.php
+++ order.php
@@ -1,10 +1,17 @@
 <?php
-	$User=$_GET['name'].$_GET['pass'];
-	$MyUser='zhangsan';
-	$MyPass='123123';
-	if($User==$MyUser.$MyPass){
-		echo 'ok';
-	}else{
-		echo 'error';
-	}
+   header("content-type:text/html;charset=utf-8");
+   $GetUser=$_POST['user'];
+   $GetPassWord=$_POST['pass'];
+// $GetPhone=$_POST['phone'];
+   $MyUser='zhangsan';
+   $MyPassWord='123456';
+// $MyPhone="12345678912";
+   
+    if($GetUser==$MyUser&&$GetPassWord==$MyPassWord){
+     	echo '登录成功';
+    }else if($GetUser!=$MyUser&&$GetPassWord!=$MyPassWord){
+        echo '账号或密码输入错误,请重新输入';
+    }else{
+    	echo "请重新输入正确的账号或密码";
+    }
 ?>
